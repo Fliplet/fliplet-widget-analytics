889fd1776cd8d45da40dc9f8c7e97719f80b58c1
Nicholas Valbusa <nvalbusa@weboo.biz>
2018-08-09 11:50:39 +0200

    script to track page views and events

--- js/build.js
+++ js/build.js
@@ -1,4 +1,16 @@
 (function () {
-  // TODO:
-  // Get script from html to thi file once we can include assets on the head scripts
+  // Screen data capture  
+  Fliplet.App.Analytics.pageView({
+    screen: Fliplet.Env.get('pageTitle')
+  });
+  
+  // Intercepts events
+  Fliplet.Analytics.subscribe('trackEvent', function (event) {
+    Fliplet.App.Analytics.event({
+      category: event.category,
+      action: event.action,
+      label: event.label,
+      screen: Fliplet.Env.get('pageTitle') + ' / ' + event.action + ' / ' + event.label
+    });
+  });
 })();
