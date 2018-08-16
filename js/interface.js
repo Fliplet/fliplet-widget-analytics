191981cb8e88340e1eb181ac91de4949e00920f9
Tony Wu <twu@fliplet.com>
2018-08-16 11:32:48 +0100

    Updates interface to save configuration

--- js/interface.js
+++ js/interface.js
@@ -1,2 +1,19 @@
 Fliplet.Widget.autosize();
-Fliplet.Studio.emit('widget-save-label-update', {text: ''});
+
+$('form').submit(function (event) {
+  event.preventDefault();
+
+  Fliplet.Widget.save({
+    trackUsers: !$('#disable-user-tracking').is(':checked')
+  }).then(function () {
+    Fliplet.Widget.complete();
+  });
+});
+
+// Fired from Fliplet Studio when the external save button is clicked
+Fliplet.Widget.onSaveRequest(function () {
+  $('form').submit();
+});
+
+var data = Fliplet.Widget.getData();
+$('#disable-user-tracking').prop('checked', data.trackUsers === false);
