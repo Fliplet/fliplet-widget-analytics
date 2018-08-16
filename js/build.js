3d6826ccfede7f650c2bf9b984addd192dded8df
Tony Wu <twu@fliplet.com>
2018-08-16 11:40:09 +0100

    Removes checking of Fliplet.Analytics.isTrackingEnabled
    This is centrally done via Fliplet.Analytics in fliplet-core

--- js/build.js
+++ js/build.js
@@ -3,34 +3,27 @@
     return; // do not track in edit mode  
   }
 
-  Fliplet.Analytics.isTrackingEnabled()
-    .then(function (enabled) {
-      if (!enabled) {
-        return;
-      }
+  var pageTitle = Fliplet.Env.get('pageTitle');
+  if (Fliplet.App.isPreview(true)) {
+    pageTitle += ' (Preview)';
+  }
 
-      var pageTitle = Fliplet.Env.get('pageTitle');
-      if (Fliplet.App.isPreview(true)) {
-        pageTitle += ' (Preview)';
-      }
-
-      // Wait until app hooks have been fired to avoid tracking the page view
-      // when a security hook redirects the user to a different screen
-      Fliplet.Hooks.on('beforePageViewHooksSuccess', function () {
-        // Screen data capture
-        Fliplet.App.Analytics.pageView({
-          screen: pageTitle
-        });
-      });
-      
-      // Intercepts events
-      Fliplet.Analytics.subscribe('trackEvent', function (event) {
-        Fliplet.App.Analytics.event({
-          category: event.category,
-          action: event.action,
-          label: event.label,
-          screen: pageTitle
-        });
-      });
+  // Wait until app hooks have been fired to avoid tracking the page view
+  // when a security hook redirects the user to a different screen
+  Fliplet.Hooks.on('beforePageViewHooksSuccess', function () {
+    // Screen data capture
+    Fliplet.App.Analytics.pageView({
+      screen: pageTitle
     });
+  });
+  
+  // Intercepts events
+  Fliplet.Analytics.subscribe('trackEvent', function (event) {
+    Fliplet.App.Analytics.event({
+      category: event.category,
+      action: event.action,
+      label: event.label,
+      screen: pageTitle
+    });
+  });
 })();
