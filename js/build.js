5c04648c87c9b8a896c3a92746a9d7c72a5beb5d
Tony Wu <twu@fliplet.com>
2018-08-16 11:22:25 +0100

    Allows optional programmatic disabling of analytics

--- js/build.js
+++ js/build.js
@@ -2,23 +2,35 @@
   if (Fliplet.Env.get('interact')) {
     return; // do not track in edit mode  
   }
-    
-  // Wait until app hooks have been fired to avoid tracking the page view
-  // when a security hook redirects the user to a different screen
-  Fliplet.Hooks.on('beforePageViewHooksSuccess', function () {
-    // Screen data capture  
-    Fliplet.App.Analytics.pageView({
-      screen: Fliplet.Env.get('pageTitle')
+
+  Fliplet.Analytics.isTrackingEnabled()
+    .then(function (enabled) {
+      if (!enabled) {
+        return;
+      }
+
+      var pageTitle = Fliplet.Env.get('pageTitle');
+      if (Fliplet.App.isPreview(true)) {
+        pageTitle += ' (Preview)';
+      }
+
+      // Wait until app hooks have been fired to avoid tracking the page view
+      // when a security hook redirects the user to a different screen
+      Fliplet.Hooks.on('beforePageViewHooksSuccess', function () {
+        // Screen data capture
+        Fliplet.App.Analytics.pageView({
+          screen: pageTitle
+        });
+      });
+      
+      // Intercepts events
+      Fliplet.Analytics.subscribe('trackEvent', function (event) {
+        Fliplet.App.Analytics.event({
+          category: event.category,
+          action: event.action,
+          label: event.label,
+          screen: pageTitle
+        });
+      });
     });
-  });
-  
-  // Intercepts events
-  Fliplet.Analytics.subscribe('trackEvent', function (event) {
-    Fliplet.App.Analytics.event({
-      category: event.category,
-      action: event.action,
-      label: event.label,
-      screen: Fliplet.Env.get('pageTitle')
-    });
-  });
 })();
